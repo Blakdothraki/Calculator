608ea03c5636b9388b5f4a2e4d5c9f07322a1966
Blakdothraki <blakdothraki@github.com>
2022-11-26 01:33:11 +0100

    Initial commit

---
+++ app/src/main/java/com/softwork/calculator/CalculatorButton.kt
@@ -0,0 +1,13 @@
+package com.softwork.calculator
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CalculatorButton(
+    symbol: String,
+    modifier: Modifier,
+    onClick: () -> Unit
+) {
+    
+}
